6a58fae9ce05d0437022f5cbde33ff367c91f14a
= <=>
2021-10-19 13:55:02 +0000

    Essaies sur l'amélioration de réponses

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.core import paginator
 from django.http.response import JsonResponse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,12 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib import request as rq
-import requests
+# import requests
 from PIL import Image
 from bs4 import BeautifulSoup
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from store.models import Image
+import random
 import traceback
 from django.contrib.auth.models import User
 # from .forms import ContactForm
@@ -26,8 +28,8 @@ def index(request):
 def listing(request):
     # Initialisation du driver 
     images_list = Image.objects.all().order_by('id')
-    paginator = Paginator(images_list, 9    )
-    page = request.GET.get('page')
+    paginator = Paginator(images_list, 9)
+    page = request.GET.get('page', 1)
     try:
         images = paginator.page(page)
     except PageNotAnInteger:
@@ -40,58 +42,94 @@ def listing(request):
         }
     return render(request, 'store/listing.html', context)
  
-def search(request):
-    query = request.GET.get('query')
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(executable_path="C:\Program Files\chromedriver\chromedriver.exe", options=options)
-    url = "https://www.google.sn/imghp?hl=fr&ogbl"
-    driver.get(url)
-    images_list = []
-    search = driver.find_element_by_tag_name('input')
-    search.clear()
-    search.send_keys(query)
-    search.send_keys(Keys.ENTER)
-    final_imgs = []
-    content = driver.page_source
-    try:
-        doc = BeautifulSoup(content, 'lxml')
-        imgs = []
-        for a in doc.find('div', class_ = "mJxzWe").find_all('a', class_ = ['wXeWr', 'islib', 'nfEiy']):
-            src = a.find('img')
-            imgs.append((a.find('img'), a.nextSibling))
-            try:
-                image = driver.find_element_by_css_selector("img[src = '{}']".format(src['src']))
-                image.click()
-                sous_lien = WebDriverWait(driver, 2).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, "a.lxa62b.MIdC8d.So4Urb"))
-                )
-                sous_lien.click()
-                content = driver.page_source
-                doc = BeautifulSoup(content, 'lxml')
-                imgs.extend([(a.find('img'), a.nextSibling) for a in doc.find('div', \
-                    class_ = "mJxzWe").find_all('a', class_ = ['wXeWr', 'islib', 'nfEiy'])])
-                driver.back()
-                driver.back()
-            except Exception:
-                pass
-        for img in imgs:
-            try:
-                if Image.objects.filter(title = img[1]['title']).exists(): 
-                    pass
-                else :
-                    rq.urlopen(img[0]['src'])
-                    images_list.append({"src" : img[0]['src'], "title": img[1]['title'], "categorie": query, "url": img[1]['href']})
-            except Exception:
-                pass
-    except Exception as e:
-        raise Exception("Erreur trouvée : %s"%(e))
+# def pagination(images_list):
     
+
+def search(request):
+    if request.method == "GET":
+        query = request.GET.get('query', '')
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(executable_path="C:\Program Files\chromedriver\chromedriver.exe", options=options)
+        url = "https://www.google.sn/imghp?hl=fr&ogbl"
+        driver.get(url)
+        images_list = []
+        search = driver.find_element_by_tag_name('input')
+        search.clear()
+        search.send_keys(query)
+        search.send_keys(Keys.ENTER)
+        content = driver.page_source
+        try:
+            doc = BeautifulSoup(content, 'lxml')
+            imgs = []
+            links = [a for a in doc.find('div', class_ = "mJxzWe").find_all('a', class_ = ['wXeWr', 'islib', 'nfEiy'])]
+            for a in links:
+                src = a.find('img')
+                imgs.append((a.find('img'), a.nextSibling))
+                try:
+                    image = driver.find_element_by_css_selector("img[src = '{}']".format(src['src']))
+                    image.click()
+                    try:
+                        sous_lien = WebDriverWait(driver, 0.6).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, "a.lxa62b.MIdC8d.So4Urb"))
+                        ) 
+                        # sous_lien = driver.find_element_by_css_selector("a.lxa62b.MIdC8d.So4Urb")
+                        sous_lien.click()
+                        content = driver.page_source
+                        doc = BeautifulSoup(content, 'lxml')
+                        imgs.extend([(a.find('img'), a.nextSibling) for a in doc.find('div', \
+                            class_ = "mJxzWe").find_all('a', class_ = ['wXeWr', 'islib', 'nfEiy'])])
+                        driver.back()
+                    except Exception:
+                        pass   
+                    driver.back()
+                except Exception:
+                    pass
+                if imgs.__len__() > 60:
+                    break
+            # for i in range(200):
+            #     content = driver.page_source
+            #     doc = BeautifulSoup(content, 'lxml')
+            #     links = [a for a in doc.find('div', class_ = "mJxzWe").find_all('a', class_ = ['wXeWr', 'islib', 'nfEiy'])]
+            #     link = random.choice(links)
+            #     src = link.find('img')
+            #     imgs.append((link.find('img'), link.nextSibling))
+            #     try:
+            #         image = driver.find_element_by_css_selector("img[src = '{}']".format(src['src']))
+            #         image.click()
+            #         sous_lien = WebDriverWait(driver, 0.6).until(
+            #             EC.presence_of_element_located((By.CSS_SELECTOR, "a.lxa62b.MIdC8d.So4Urb"))
+            #         ) 
+            #         # sous_lien = driver.find_element_by_css_selector("a.lxa62b.MIdC8d.So4Urb")
+            #         sous_lien.click()
+            #         content = driver.page_source
+            #     except Exception:
+            #         pass
+            
+            for img in imgs:
+                try:
+                    if Image.objects.filter(title = img[1]['title']).exists(): 
+                        pass
+                    else :
+                        rq.urlopen(img[0]['src'])
+                        images_list.append({"src" : img[0]['src'], "title": img[1]['title'], "categorie": query, "url": img[1]['href']})
+                except Exception:
+                    pass
+        except Exception as e:
+            raise Exception("Erreur trouvée : %s"%(e))
+    # page = request.GET.get('page', 1)
+    # paginator = Paginator(images_list, 9)
+    # try:
+    #     images = paginator.page(page)
+    # except PageNotAnInteger:
+    #     images = paginator.page(1)
+    # except EmptyPage:
+    #     images = paginator.page(paginator.num_pages)
     title = "%d résultats pour la recherche '%s'"%(len(images_list), query)
     context = {
         'images': images_list,
-        'paginate': True,
         'title': title,
+        # 'paginate': True
     }
     return render(request, 'store/search.html', context)
     
